dc1a2fe971f27e0a58ef2a44390cdd5e6279f281
M-Dan814 <m.dayan5689@gmail.com>
2023-01-03 13:36:14 -0800

    Add images for cards and create array for mapping

---
+++ src/components/cards.js
@@ -0,0 +1,68 @@
+const cards = [
+  {
+    name: "Hijikata",
+    img: require("./images/Hijikata.jpg"),
+  },
+  {
+    name: "Kamui",
+    img: require("./images/Kamui.jpg"),
+  },
+  {
+    name: "Mutsu",
+    img: require("./images/Mutsu.jpg"),
+  },
+  {
+    name: "Nobume",
+    img: require("./images/Nobume.jpg"),
+  },
+  {
+    name: "Otae",
+    img: require("./images/Otae.jpg"),
+  },
+  {
+    name: "Sakamoto",
+    img: require("./images/Sakamoto.jpeg"),
+  },
+  {
+    name: "Sarutobi",
+    img: require("./images/Sarutobi.jpg"),
+  },
+  {
+    name: "Takasugi",
+    img: require("./images/Takasugi.jpg"),
+  },
+  {
+    name: "Kondo",
+    img: require("./images/Kondo.jpg"),
+  },
+  {
+    name: "Sogo",
+    img: require("./images/Sogo.jpg"),
+  },
+  {
+    name: "Tama",
+    img: require("./images/Tama.jpg"),
+  },
+  {
+    name: "Shogun",
+    img: require("./images/Shogun.jpg"),
+  },
+  {
+    name: "Zenzo",
+    img: require("./images/Zenzo.jpg"),
+  },
+  {
+    name: "Utsuro",
+    img: require("./images/Utsuro.jpg"),
+  },
+  {
+    name: "Shinpachi",
+    img: require("./images/Shinpachi.jpg"),
+  },
+  {
+    name: "Kagura",
+    img: require("./images/Kagura.jpg"),
+  },
+];
+
+export { cards };
